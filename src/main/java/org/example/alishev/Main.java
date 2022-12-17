8e0b7d71ca9d46b8f5fce1d0bd7dfbe2ee182bb7
Shahman Magomadov <magomadovshahman@gmail.com>
2022-12-17 14:36:47 +0300

    lesson 10

---
+++ src/main/java/org/example/alishev/Main.java
@@ -0,0 +1,15 @@
+package org.example.alishev;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+				"applicationContext.xml"
+		);
+
+		Computer computer = context.getBean("computer", Computer.class);
+		System.out.println(computer);
+		context.close();
+	}
+}
